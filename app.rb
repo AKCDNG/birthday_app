f70db8c1691e9d9ed4205c6e0f8043ea16aeb8f8
Anish <anishkakaiya@outlook.com>
2022-05-13 13:04:51 +0100

    add redirect to birthday webpage with name entry

--- app.rb
+++ app.rb
@@ -6,8 +6,20 @@ class BirthdayApp < Sinatra::Base
     register Sinatra::Reloader
   end
 
+  enable :sessions
+
   get '/' do
     erb :index
+  end
+
+  post '/name' do
+    session[:name] = params[:name]
+    redirect '/birthday'
+  end
+
+  get '/birthday' do
+    @name = session[:name]
+    erb :birthday
   end
 
   run! if app_file == $0
